d3a259a6e3102d552c01e6bb510a0f1b657ff995
Maxim Nisenblat <P0024891@ness-tech.co.il>
2019-08-12 10:56:07 +0300

    update

--- index.js
+++ index.js
@@ -1,12 +1,6 @@
 const fs = require("fs");
 const compiled = new WebAssembly.Module(fs.readFileSync(__dirname + "/build/optimized.wasm"));
-const imports = {
-  env: {
-    abort(_msg, _file, line, column) {
-       console.error("abort called at index.ts:" + line + ":" + column);
-    }
-  }
-};
+const imports = {};
 Object.defineProperty(module, "exports", {
   get: () => new WebAssembly.Instance(compiled, imports).exports
 });
